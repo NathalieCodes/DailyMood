a82cb7b4db3de6231a51b294b5d82e601a67176e
Jérôme Gangneux <jerome@gangneux.net>
2018-11-12 14:08:30 -0800

    Create register.php

---
+++ register.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+</head>
+<body>
+<div class="form-wrapper">
+    <form action="register_action.php" method="post">
+        <label>
+            Username:
+            <input type="text" name="username" value="">
+        </label>
+        <label>
+            Password:
+            <input type="password" name="password" value="">
+        </label>
+    </form>
+</div>
+</body>
+</html>
