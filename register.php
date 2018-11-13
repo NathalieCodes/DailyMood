a8a6a1d34d6cbbb8e74ea0f4dfb4eb0662853475
Jérôme Gangneux <jerome@gangneux.net>
2018-11-12 19:03:01 -0800

    Update register.php

--- register.php
+++ register.php
@@ -1,9 +1,42 @@
-<html>
+<?php
+$error = $_GET["error"];
+$errorMessage = "";
+switch ($error) {
+    case 1: 
+        $errorMessage = "Username is too short!";
+        break;
+        
+    case 2: 
+        $errorMessage = "Password is to short, use at least 8 caracters";
+        break;
+
+    case 3: 
+        $errorMessage = "You can not use the same username and password";
+        break;
+
+    case 4: 
+        $errorMessage = "Please don't use this word in your username";
+        break;
+
+    case 5: 
+        $errorMessage = "This username is already used";
+        break;
+
+    case 6: 
+        $errorMessage = "Oops an unknown error happened...";
+        break;
+}
+?><html>
 <head>
 </head>
 <body>
 <div class="form-wrapper">
     <form action="register_action.php" method="post">
+        <?php
+        if ($errorMessage != "") {
+            echo "<span class=\"error\">$errorMessage</span>";    
+        }
+        ?>
         <label>
             Username:
             <input type="text" name="username" value="">
